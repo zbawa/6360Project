911bd874db8ffd3f7cfa7d345d649949c7117637
zsb <zamaanb@gmail.com>
2022-12-05 11:57:53 -0600

    Linked db

--- server.js
+++ server.js
@@ -1,15 +1,51 @@
 var express = require('express');
-var path = require('path');
+const path = require('path');
+const mysql = require('mysql');
+var hbs = require('hbs');
 var app = express();
 let port = process.env.PORT;
 
-app.use(express.static(path.join(__dirname, './src')));
+app.set('views', path.join(__dirname, './src/views'))
+app.set('view engine', 'hbs')
 
-app.get('/', function(req, res) {
-    res.redirect('index.html');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "test_db1"
 });
 
-if (port == null || port == ""){
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("DB Connection Successful");
+})
+
+app.get('/', (req, res) => {
+    con.query("SELECT * FROM Trader", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        res.get(result[0].First_Name + " \n" +
+            result[0].Last_Name + " \n" +
+            result[0].Email + " \n" +
+            result[0].Phone_Number + " \n" +
+            result[0].Cellphone_Number + " \n" +
+            result[0].Ethereum_Amount + " " +
+            result[0].Ethereum_Address + " \n" +
+            result[0].Tier
+        );
+        var wallet = result[0].Ethereum_Amount;
+        res.render('demo', {wallet});
+    })
+});
+
+var Ethereum_Amount;
+
+app.get('/', function (req, res) {
+    res.redirect('index.html');
+});
+app.use(express.static(path.join(__dirname, './src')));
+
+if (port == null || port == "") {
     port = 8000;
 }
 app.listen(port); //the port you want to use
