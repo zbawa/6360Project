860c3b6a59d8a00e265948bd47c8d0fa9003e207
zsb <zamaanb@gmail.com>
2022-12-06 19:59:54 -0600

    added login feature

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mysql = require('mysql');
 var hbs = require('hbs');
 var app = express();
+const session = require('express-session');
 let port = process.env.PORT;
 
 app.set('views', path.join(__dirname, './src/views'))
@@ -15,6 +16,14 @@ var con = mysql.createConnection({
     database: "test_db1"
 });
 
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 con.connect(function (err) {
     if (err) throw err;
     console.log("DB Connection Successful");
@@ -22,41 +31,44 @@ con.connect(function (err) {
 
 app.get('/app/home/home.html', (req, res) => {
     var sql = "SELECT * FROM Trader, owned_nft";
-    con.query(sql, function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        res.get(result[0].Trader_ID + " \n" + result[0].First_Name + " \n" +
-            result[0].Last_Name + " \n" +
-            result[0].Email + " \n" +
-            result[0].Phone_Number + " \n" +
-            result[0].Cellphone_Number + " \n" +
-            result[0].Ethereum_Amount + " " +
-            result[0].Ethereum_Address + " \n" +
-            result[0].Tier + " \n" +
-            result[0].NFT_Token_ID + " \n" +
-            result[0].NFT_Name + " \n" +
-            result[0].Market_Price_Ethereum + " \n" +
-            result[0].Market_Price_USD
-        );
 
-        var trader1 = {
-            tID: result[0].Trader_ID,
-            fName: result[0].First_Name,
-            lName: result[0].Last_Name,
-            email: result[0].Email,
-            phone: result[0].Phone_Number,
-            cell: result[0].Cellphone_Number,
-            wallet: result[0].Ethereum_Amount,
-            address: result[0].Ethereum_Address,
-            tier: result[0].Tier,
-            nftTokenID: result[0].NFT_Token_ID,
-            nftName: result[0].NFT_Name,
-            ethPrice: result[0].Market_Price_Ethereum,
-            usdPrice: result[0].Market_Price_USD
-        }
-
-        res.render('home', { trader1 });
-    })
+    if (req.session.loggedin) {
+        con.query(sql, function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            res.get(result[0].Trader_ID + " \n" + result[0].First_Name + " \n" +
+                result[0].Last_Name + " \n" +
+                result[0].Email + " \n" +
+                result[0].Phone_Number + " \n" +
+                result[0].Cellphone_Number + " \n" +
+                result[0].Ethereum_Amount + " " +
+                result[0].Ethereum_Address + " \n" +
+                result[0].Tier + " \n" +
+                result[0].NFT_Token_ID + " \n" +
+                result[0].NFT_Name + " \n" +
+                result[0].Market_Price_Ethereum + " \n" +
+                result[0].Market_Price_USD
+            );
+    
+            var trader1 = {
+                tID: result[0].Trader_ID,
+                fName: result[0].First_Name,
+                lName: result[0].Last_Name,
+                email: result[0].Email,
+                phone: result[0].Phone_Number,
+                cell: result[0].Cellphone_Number,
+                wallet: result[0].Ethereum_Amount,
+                address: result[0].Ethereum_Address,
+                tier: result[0].Tier,
+                nftTokenID: result[0].NFT_Token_ID,
+                nftName: result[0].NFT_Name,
+                ethPrice: result[0].Market_Price_Ethereum,
+                usdPrice: result[0].Market_Price_USD
+            }
+    
+            res.render('home', { trader1 });
+        })
+    }   
 });
 
 app.get('/app/available-nfts/available-nfts.html', function (req, res) {
@@ -125,9 +137,37 @@ app.get('/app/features/history.html', function (req, res) {
 });
 
 
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+    console.log(username);
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		con.query('SELECT T.Email, C.Password  FROM Trader T, Credentials C WHERE T.Email = ? AND C.Password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/app/home/home.html');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
 
 app.get('/', function (req, res) {
-    res.redirect('index.html');
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 app.use(express.static(path.join(__dirname, './src')));
 
